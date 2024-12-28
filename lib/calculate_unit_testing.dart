b76fccb062f74248c2cdaa07101fef50498e5e84
MohamedMohamedSaleh <alrisha77777@gmail.com>
2024-12-28 18:24:46 +0200

    Add Testing

---
+++ lib/calculate_unit_testing.dart
@@ -0,0 +1,16 @@
+library calculate_unit_testing;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [num1] plus [num2].
+  int add(int num1, int num2) => num1 + num2;
+
+  /// Returns [num1] minus [num2].
+  int subtract(int num1, int num2) => num1 - num2;
+
+  /// Returns [num1] multiplied by [num2].
+  int multiply(int num1, int num2) => num1 * num2;
+
+  /// Returns [num1] divided by [num2].
+  double divide(int num1, int num2) => num1 / num2;
+}
---
+++ test/calculate_unit_testing_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:calculate_unit_testing/calculate_unit_testing.dart';
+
+void main() {
+  test("Add two numbers if num1 is 4 and num2 is 5 then return 9", () {
+    ///AAA
+
+    ///Arrange
+    final calculator = Calculator();
+    const num1 = 4;
+    const num2 = 5;
+    const expected = 9;
+
+    ///Act
+    final result = calculator.add(num1, num2);
+
+    ///Assert
+    expect(result, expected);
+  });
+}
